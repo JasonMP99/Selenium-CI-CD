5b1b171c23390191deff48f6c8601821a5a59328
Jason <jason29899@gmail.com>
2024-04-02 22:28:17 +0300

    Initial files commit

---
+++ src/test/java/rahulshettyacademy/tests/ErrorValidationsTest.java
@@ -0,0 +1,41 @@
+package rahulshettyacademy.tests;
+
+import org.testng.annotations.Test;
+import org.testng.Assert;
+
+import rahulshettyacademy.TestComponents.BaseTest;
+import rahulshettyacademy.TestComponents.Retry;
+import rahulshettyacademy.pageobjects.CartPage;
+import rahulshettyacademy.pageobjects.ProductCatalogue;
+
+public class ErrorValidationsTest extends BaseTest {
+
+	@Test(groups= {"ErrorHandling"},retryAnalyzer=Retry.class)
+	public void LoginErrorValidation(){
+
+	
+		landingPage.loginApplication("anshika@gmail.com", "Iamki000");
+		Assert.assertEquals("Incorrect email or password.", landingPage.getErrorMessage());
+
+	}
+	
+
+	@Test
+	public void ProductErrorValidation() throws InterruptedException
+	{
+
+		String productName = "ADIDAS ORIGINAL";
+		ProductCatalogue productCatalogue = landingPage.loginApplication("rahulshetty@gmail.com", "Iamking@000");
+		productCatalogue.addProductToCart(productName);
+		CartPage cartPage = productCatalogue.goToCartPage();
+		Boolean match = cartPage.VerifyProductDisplay("ZARA COAT 33");
+		Assert.assertFalse(match);
+		
+	
+
+	}
+
+	
+	
+
+}
